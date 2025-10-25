d325d9bd5e991992eac084c2dbe398c8d1fb497a
Gabriel mousquer dos santos <gabrielmousquer1227@gmail.com>
2025-10-24 21:07:14 -0300

    ✨ Configura eslint e adiciona prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,24 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'no-unused-vars': 'error',
+      'new-cap': 'off',
+      'no-invalid-this': 'off',
+      'require-jsdoc': 'off',
+      'valid-jsdoc': 'off',
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
