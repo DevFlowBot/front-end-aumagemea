12dff970e1151f61a9e8f6ad0f042a7f22a7cef7
Gabriel mousquer dos santos <gabrielmousquer1227@gmail.com>
2025-10-25 19:16:53 -0300

    🎨 Format code

--- src/components/commom/header.jsx
+++ src/components/commom/header.jsx
@@ -7,6 +7,7 @@ export function Header() {
   return (
     <Flex
       as="header"
+      w="90%"
       maxW="1193px"
       p={5}
       mt={10}
@@ -16,38 +17,49 @@ export function Header() {
       bg="white"
       borderRadius="100px"
       boxShadow="0 8px 30px rgba(0, 0, 0, 0.3)"
+      wrap="wrap"
     >
-      <Heading size="md">
+      <Heading size="md" flexShrink={0}>
         <Link href="/">
-        <Image
-          src="/logo.png"
-          alt="logo"
-          maxW={{ base: '49px', md: '49px', lg: '64px' }}
-          h="auto"
-          objectFit="contain"
-        />
+          <Image
+            src="/logo.png"
+            alt="logo"
+            maxW={{ base: '40px', md: '49px', lg: '64px' }} // responsivo
+            h="auto"
+            objectFit="contain"
+          />
         </Link>
       </Heading>
 
-      <HStack spacing={3}>
+      <HStack
+        spacing={3}
+        flexWrap="wrap"
+        justify={{ base: 'center', md: 'flex-start' }}
+        my={{ base: 2, md: 0 }}
+      >
         <Link href="/">
-          <Text color="#730E45">Inicio</Text>
+          <Text color="#730E45" cursor="pointer">
+            Inicio
+          </Text>
         </Link>
-
         <Link href="/blog">
-          <Text color="#730E45">Blog</Text>
+          <Text color="#730E45" cursor="pointer">
+            Blog
+          </Text>
         </Link>
-        
         <Link href="/adote">
-          <Text color="#730E45">Audote</Text>
+          <Text color="#730E45" cursor="pointer">
+            Audote
+          </Text>
         </Link>
-        
         <Link href="/parceiros">
-          <Text color="#730E45">Parceiros</Text>
+          <Text color="#730E45" cursor="pointer">
+            Parceiros
+          </Text>
         </Link>
       </HStack>
 
-      <HStack spacing={3}>
+      <HStack spacing={3} flexWrap="wrap" my={{ base: 2, md: 0 }}>
         <Link href="/cadastro">
           <Text color="#730E45" cursor="pointer">
             Cadastre-se
