8c62ccf61735eba2fcbcbbbaf9389d6c4c4bc611
gabrieligardini <gabrieli.gardini9463@gmail.com>
2025-10-29 22:25:15 -0300

    Criação Aba de Parceiros

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Flex, Box, Button, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 import { Footer, Wave, BlogCard } from '@/components/commom';
 import blogData from '../../fakedata/blog.json' assert { type: 'json' };
 
@@ -12,6 +13,7 @@ export default function HomePage() {
       </Flex>
 
       <BlogWithWave />
+      <ParceirosSection />
       <FooterWithWave />
     </Flex>
   );
@@ -21,7 +23,7 @@ export const BlogWithWave = () => {
   return (
     <Box w="100%" position="relative" zIndex={0}>
       <Flex>
-        <Wave color="#FFECC8" mirror />
+        <Wave color="#FFECC8" />
       </Flex>
 
       <Flex
@@ -50,7 +52,7 @@ export const BlogWithWave = () => {
       </Flex>
 
       <Flex>
-        <Wave color="#FFECC8" invert />
+        <Wave color="#FFECC8" invert mirror />
       </Flex>
     </Box>
   );
@@ -84,7 +86,8 @@ const BlogDataCard = () => {
           color="#AB678D"
           borderRadius="full"
           size="lg"
-          px={8}
+          px={10} 
+          py={6}
           _hover={{
             bg: '#FFECC8',
             color: '#AB678D',
@@ -98,6 +101,58 @@ const BlogDataCard = () => {
     </Flex>
   );
 };
+
+const ParceirosSection = () => {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      minH="90vh"
+      py={{ base: 24, md: 45 }}
+      mt={{ base: -8, md: -10 }}
+      mb={{ base: 12, md: 40 }}
+    >
+      <Text
+        fontSize={{ md: '40px' }}
+        fontWeight="500"
+        mb={12}
+        fontFamily="Poppins"
+        color="#8D3767"
+      >
+        Parceiros
+      </Text>
+
+      <Flex
+        wrap="wrap"
+        justify="center"
+        gap={{ base: 6, md: 48 }}
+        align="center"
+      >
+        <Box
+          w={{ base: '200px', md: '387px' }}
+          h={{ base: '175px', md: '339px' }}
+          position="relative">
+          <Image
+            src="/logos/sampa.png"
+            alt="Sampa"
+            fill style={{ objectFit: 'contain' }} />
+        </Box>
+
+        <Box
+          w={{ base: '200px', md: '387px' }}
+          h={{ base: '175px', md: '339px' }}
+          position="relative">
+          <Image
+            src="/logos/artemys.png"
+            alt="Artemys"
+            fill style={{ objectFit: 'contain' }} />
+        </Box>
+      </Flex>
+    </Flex>
+  );
+};
+
 
 const FooterWithWave = () => {
   return (
